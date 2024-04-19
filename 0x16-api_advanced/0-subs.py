7b8dd93c62bb2476485489120465256d27c68949
Lewis Hinga <123990686+lewisjn-arch@users.noreply.github.com>
2024-04-19 22:02:47 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,17 +2,13 @@
 import requests
 
 def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'MyBot/0.1'}  # Set a custom User-Agent to avoid errors
-    
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            subscribers = data['data']['subscribers']
-            return subscribers
-        else:
-            return 0
-    except Exception as e:
-        print("An error occurred:", e)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
         return 0
