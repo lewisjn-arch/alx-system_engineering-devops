fc27a65385a84399be56e7fab0a524dbcefb4060
Lewis Hinga <123990686+lewisjn-arch@users.noreply.github.com>
2024-04-19 22:08:01 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,19 @@
+#!/usr/bin/python3
+"""
+Script that queries subscribers on a given Reddit subreddit.
+"""
+
 import requests
+
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
-    
-    try:
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()  # Raise an exception for HTTP errors
-        
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
         data = response.json()
-        if 'data' in data and 'subscribers' in data['data']:
-            subscribers = data['data']['subscribers']
-            return subscribers
-        else:
-            return 0
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
         return 0
